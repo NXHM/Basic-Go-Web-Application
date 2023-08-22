8130069ce5162b92f93c01d7045fbcb090e86c6f
Nicolas Xavier Herrera Medina <nicoxavier2013@hotmail.com>
2023-08-21 19:29:58 -0500

    Agregar net

--- wiki.go
+++ wiki.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
+}
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
