79cbffacba19353d3f461cfffc7d698d47fae641
Nicolas Xavier Herrera Medina <nicoxavier2013@hotmail.com>
2023-10-10 15:26:07 -0500

    Termine la guía

--- wiki.go
+++ wiki.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 
@@ -15,16 +18,22 @@ func main() {
 	defer file_error.Close()                   // Cierra el archivo Error.txt al finalizar la ejecución del programa
 	log.SetOutput(file_error)                  // Escribe los logs en el archivo Error.txt
 	// http.HandleFunc("/", handler)          // Le dice a http que maneje todas las requests a / con handler
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
 	// Uso select case segun el path que se coloque en el navegador
-	err := http.ListenAndServe(":8080", nil)
 	// En caso que haya un error, se anota el error
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-		os.Exit(1)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("invalid Page Title")
 	}
+	return m[2], nil // The title is the second subexpression.
 }
 
 type Page struct {
@@ -32,8 +41,17 @@ type Page struct {
 	Body  []byte
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
 	err := p.save()
@@ -43,20 +61,21 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	t, _ := template.ParseFiles("view.html")
-	t.Execute(w, p)
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
 }
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	t, _ := template.ParseFiles("edit.html")
-	t.Execute(w, p)
+	renderTemplate(w, "edit", p)
 }
 func writeErrorsFile(filename string) *os.File {
 	// Crea Fila del objeto os.File para escribir logs y err en caso que hayan errores
@@ -66,14 +85,37 @@ func writeErrorsFile(filename string) *os.File {
 	}
 	return file
 }
-func handler(w http.ResponseWriter, r *http.Request) { // w es la respuesta que se le envía al cliente, r es el request que se recibe del cliente y r es la solicitud
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+
+/*
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) { //Carga el template edit.html y carga los datos de la pagina p
+
+		t, err := template.ParseFiles(tmpl + ".html")
+	    if err != nil {
+	        http.Error(w, err.Error(), http.StatusInternalServerError)
+	        return
+	    }
+	    err = t.Execute(w, p)
+	    if err != nil {
+	        http.Error(w, err.Error(), http.StatusInternalServerError)
+	    }
+	}
+*/
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
-func (p *Page) save() error {
+
+/*func handler(w http.ResponseWriter, r *http.Request) { // w es la respuesta que se le envía al cliente, r es el request que se recibe del cliente y r es la solicitud
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}// Se usa fmt para este*/
+
+func (p *Page) save() error { //Guarda la pagina en un archivo
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
 }
-func loadPage(title string) (*Page, error) {
+func loadPage(title string) (*Page, error) { // Carga la pagina desde un archivo
 	filename := title + ".txt"
 	body, err := os.ReadFile(filename)
 	if err != nil {
