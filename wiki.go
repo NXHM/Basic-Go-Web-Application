2b95250f83857bc6c09abca557ef728e73735264
Nicolas Xavier Herrera Medina <nicoxavier2013@hotmail.com>
2023-10-10 13:45:05 -0500

    Agregar distintos end-points y un logger

--- wiki.go
+++ wiki.go
@@ -2,21 +2,29 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
 
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
 func main() {
-	file_error := openLogFile("Error.txt") // Abre el archivo Error.txt
-	defer file_error.Close()               // Cierra el archivo Error.txt al finalizar la ejecución del programa
-	log.SetOutput(file_error)              // Escribe los logs en el archivo Error.txt
+	file_error := writeErrorsFile("Error.txt") // Abre el archivo Error.txt
+	defer file_error.Close()                   // Cierra el archivo Error.txt al finalizar la ejecución del programa
+	log.SetOutput(file_error)                  // Escribe los logs en el archivo Error.txt
 	// http.HandleFunc("/", handler)          // Le dice a http que maneje todas las requests a / con handler
-	//http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-	//http.HandleFunc("/save/", saveHandler)
-	// Escucha en el puerto 8080 y espera a que termine
-	log.Fatal(http.ListenAndServe(":8080", nil)) // Si es que hay un error, se anota el error
+	http.HandleFunc("/save/", saveHandler)
+	// Uso select case segun el path que se coloque en el navegador
+	err := http.ListenAndServe(":8080", nil)
+	// En caso que haya un error, se anota el error
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+		os.Exit(1)
+	}
 }
 
 type Page struct {
@@ -24,20 +32,33 @@ type Page struct {
 	Body  []byte
 }
 
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	t, _ := template.ParseFiles("view.html")
+	t.Execute(w, p)
+}
 func editHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/edit/"):]
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
-		"<form action=\"/save/%s\" method=\"POST\">"+
-		"<textarea name=\"body\">%s</textarea><br>"+
-		"<input type=\"submit\" value=\"Save\">"+
-		"</form>",
-		p.Title, p.Title, p.Body)
+	t, _ := template.ParseFiles("edit.html")
+	t.Execute(w, p)
 }
-func openLogFile(filename string) *os.File {
+func writeErrorsFile(filename string) *os.File {
 	// Crea Fila del objeto os.File para escribir logs y err en caso que hayan errores
 	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666) // (nombre del archivo,Agregar contenido|Crear si no existe|Escribir solo, permisos de escritura y lectura para el dueño, grupos y otros usuarios)
 	if err != nil {
@@ -45,7 +66,6 @@ func openLogFile(filename string) *os.File {
 	}
 	return file
 }
-
 func handler(w http.ResponseWriter, r *http.Request) { // w es la respuesta que se le envía al cliente, r es el request que se recibe del cliente y r es la solicitud
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
